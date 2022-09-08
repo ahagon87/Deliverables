d4e273231bd0de4a05b70e7ed41933f86915688a
ahagon87 <ahagon87@gmail.com>
2022-09-07 18:59:34 -0600

    commit 2

---
+++ CKK.Logic/CKK.Logic/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CKK.Logic
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
